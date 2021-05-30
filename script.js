ecb43159e31d24688a5a29a9b7c538540a07a7ec
Gavyn Holt <gavynholt@gmail.com>
2021-05-30 17:41:57 -0400

    created global timer variable timerID

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const tailHeight = parseInt($(".tail").height());
 // Size of Donkey Div
 const MAX_WIDTH = 450 - tailWidth;
 const MAX_HEIGHT = 500 - tailHeight;
+// Get the timer box to reference many times
+const timerBox = $(".timer");
+// Global timer ID
+let timerID;
 
 // Gets tail position, removes last two letters "px", and parses as an int to later modify
 // Axis values are a string of either "top" or "left"
@@ -19,57 +23,9 @@ function setTailPosition(axis, value) {
   $(".tail").css(axis, stringValue);
 }
 
-function validateEndGame() {
-  const xPos = getTailPosition("left");
-  const yPos = getTailPosition("top");
-  console.log("x: " + xPos);
-  console.log("y: " + yPos);
-  if (xPos >= 220 && xPos <= 300 && yPos >= 235 && yPos <= 315) {
-    alert("Congratulations! Great Success!");
-  } else {
-    alert("Try Again WOMP WOMP");
-  }
-}
-
-// On START click, start game timer
-$("#startGame").on("click", function () {
-  const randomX = Math.floor(Math.random() * MAX_WIDTH);
-  const randomY = Math.floor(Math.random() * MAX_HEIGHT);
-  setTailPosition("left", randomX);
-  setTailPosition("top", randomY);
-
-  // Get the timer box to reference many times
-  const timerBox = $(".timer");
-  // READY SET GO timer
-  const readyText = ["READY...", "SET...", "GO!"];
-  let i = 0;
-  timerBox.text(readyText[i]);
-  const readySetGo = window.setInterval(function () {
-    i++;
-    timerBox.text(readyText[i]);
-    if (i === readyText.length - 1) {
-      clearInterval(readySetGo);
-
-      // Display the tail on the screen
-      $(".tail").css("display", "inherit");
-
-      // Timer until game ends
-      let seconds = 10;
-      const startTimer = window.setInterval(function () {
-        timerBox.text(`Seconds Left: ${seconds}`);
-        if (seconds == 0) {
-          clearInterval(startTimer);
-          validateEndGame();
-        }
-        seconds--;
-      }, 1000);
-    }
-  }, 1000);
-});
-
 // Key press listener for up, right, down, left and space bar key presses
 // Each if statement prevents tail from going beyond the border of the Donkey Div
-$(document).keydown(function (e) {
+const keyPressListener = $(document).keydown(function (e) {
   const xPos = getTailPosition("left");
   const yPos = getTailPosition("top");
   const moveValue = 5;
@@ -92,6 +48,55 @@ $(document).keydown(function (e) {
       e.stopPropagation();
       e.preventDefault();
     }
-    console.log("space bar");
+    validateEndGame();
   }
+});
+
+function validateEndGame() {
+  window.clearInterval(timerID);
+  const xPos = getTailPosition("left");
+  const yPos = getTailPosition("top");
+  console.log("x: " + xPos);
+  console.log("y: " + yPos);
+  if (xPos >= 220 && xPos <= 300 && yPos >= 235 && yPos <= 315) {
+    alert("Congratulations! Great Success!");
+  } else {
+    alert("Try Again WOMP WOMP");
+  }
+  timerBox.text("");
+}
+
+// On START click, start game timer
+$("#startGame").on("click", function () {
+  const randomX = Math.floor(Math.random() * MAX_WIDTH);
+  const randomY = Math.floor(Math.random() * MAX_HEIGHT);
+  setTailPosition("left", randomX);
+  setTailPosition("top", randomY);
+
+  // READY SET GO timer
+  const readyText = ["READY...", "SET...", "GO!"];
+  let i = 0;
+  timerBox.text(readyText[i]);
+  const readySetGo = window.setInterval(function () {
+    i++;
+    timerBox.text(readyText[i]);
+    if (i === readyText.length - 1) {
+      clearInterval(readySetGo);
+
+      // Display the tail on the screen
+      $(".tail").css("display", "inherit");
+
+      // Timer until game ends
+      let seconds = 10;
+      const startTimer = window.setInterval(function () {
+        timerID = startTimer;
+        console.log(timerID); // set global timerID for later reference
+        timerBox.text(`Seconds Left: ${seconds}`);
+        if (seconds == 0) {
+          validateEndGame();
+        }
+        seconds--;
+      }, 1000);
+    }
+  }, 1000);
 });
